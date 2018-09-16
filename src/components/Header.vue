d52e5d753f4e1492d048058f98c4bfcfb4d8a5d1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-15 22:04:34 -0400

    Add functionality to switch routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,9 @@
 <template>
   <v-toolbar dark color="primary">
-    <v-toolbar-title class="white--text">StockFolio</v-toolbar-title>
+    <v-toolbar-title class="white--text mr-3" @click="goHome">StockFolio</v-toolbar-title>
     <v-toolbar-items>
-      <v-btn flat>Portfolio</v-btn>
-      <v-btn flat>Stocks</v-btn>
+      <v-btn flat to="/portfolio">Portfolio</v-btn>
+      <v-btn flat to="stocks">Stocks</v-btn>
     </v-toolbar-items>
     <v-spacer></v-spacer>
     <v-toolbar-items>
@@ -14,9 +14,21 @@
 </template>
 
 <script>
+import router from '../router.js'
 export default {
   data: () => ({
     items: ['Action', 'Another Action']
-  })
+  }),
+  methods: {
+    goHome: () => {
+      router.push('/')
+    }
+  }
 }
 </script>
+
+<style>
+.v-toolbar__title {
+  cursor: pointer;
+}
+</style>
