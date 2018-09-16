15e09c91ee9162ea14af102bd4904e0fc1bfce70
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-15 21:28:36 -0400

    Add nav items

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,22 @@
 <template>
   <v-toolbar dark color="primary">
     <v-toolbar-title class="white--text">StockFolio</v-toolbar-title>
+    <v-toolbar-items>
+      <v-btn flat>Portfolio</v-btn>
+      <v-btn flat>Stocks</v-btn>
+    </v-toolbar-items>
     <v-spacer></v-spacer>
-       <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-items>
+      <v-btn flat>End Day</v-btn>
+      <v-btn flat>Save & Load</v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    items: ['Action', 'Another Action']
+  })
+}
+</script>
