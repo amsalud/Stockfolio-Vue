906ceaf0f476a8ba30546ceb927fca67f90a77cd
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-15 21:12:06 -0400

    Render header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <app-header></app-header>
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: {
+    appHeader: Header
+  }
+}
+</script>
 
 <style lang="less">
 #app {
