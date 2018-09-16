97348a444dc54f536f316fddbee771b8d08bcc43
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-15 21:11:33 -0400

    Add header component toolbar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,7 @@
 <template>
-    <h1>The header component</h1>
+  <v-toolbar dark color="primary">
+    <v-toolbar-title class="white--text">StockFolio</v-toolbar-title>
+    <v-spacer></v-spacer>
+       <v-toolbar-side-icon></v-toolbar-side-icon>
+  </v-toolbar>
 </template>
