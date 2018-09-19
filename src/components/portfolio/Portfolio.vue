6dc39368554163acebda423e9296a7f9f54b6fd7
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-18 23:31:39 -0400

    Render stocks and add styles

--- src/components/portfolio/Portfolio.vue
+++ src/components/portfolio/Portfolio.vue
@@ -1,12 +1,38 @@
 <template>
-    <h1>The portfolio component</h1>
+  <v-container fluid mt-5 class="portfolio-dashboard">
+    <v-layout>
+      <app-stock v-for="stock in stocks" :stock="stock" v-bind:key="stock.id"></app-stock>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import Stock from './Stock.vue'
+
 export default {
-  name: 'Portfolio',
-  props: {
-    msg: String
+  computed: {
+    ...mapGetters({
+      stocks: 'stockPortfolio'
+    })
+  },
+  components: {
+    appStock: Stock
   }
 }
 </script>
+
+<style lang="less">
+.portfolio-dashboard {
+  .card {
+    width: 250px;
+    margin: 10px;
+    .header {
+      background-color: red;
+      color: white;
+      padding: 10px;
+      text-align: left;
+    }
+  }
+}
+</style>
