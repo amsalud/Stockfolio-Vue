3982efe447c205ba73136aa8dc81cd0ae9e97b59
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-18 21:31:21 -0400

    Add Stock component structure

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,21 @@
 <template>
-    <h1>The stocks - stock component</h1>
+    <v-flex>
+        <v-card class="card">
+            <div class="header">
+                {{ stock.name}} (Price: {{stock.price}})
+            </div>
+            <v-flex ma-3>
+                <v-text-field label="Quantity" type="number" v-model="quantity"></v-text-field>
+            </v-flex>
+            <v-btn color="success">Buy</v-btn>
+        </v-card>
+    </v-flex>
 </template>
+
+
+<script>
+export default {
+  props: ['stock'],
+  data: () => ({ quantity: 0 })
+}
+</script>
