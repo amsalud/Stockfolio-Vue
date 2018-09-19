bfa7c3a0d80ce14b323a4ea83c6f025ba4c9734a
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-18 21:22:08 -0400

    Render stock items

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,35 +1,32 @@
 <template>
-  <v-layout mt-5 class="stocks-dashboard">
-    <v-flex offset-sm3>
-      <v-card class="card">
-        <div class="header">
-          Stock Name (Price: Price)
-        </div>
-        <v-flex ma-3>
-          <v-text-field
-            label="Quantity"
-            type="number"
-          ></v-text-field>
-        </v-flex>
-        <v-btn color="success">Buy</v-btn>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container fluid align-center mt-5 class="stocks-dashboard">
+    <v-layout wrap>
+      <v-flex v-for="stock in stocks" v-bind:key="stock.id">
+        <v-card class="card">
+          <div class="header">
+            {{ stock.name}} (Price: {{stock.price}})
+          </div>
+          <v-flex ma-3>
+            <v-text-field label="Quantity" type="number"></v-text-field>
+          </v-flex>
+          <v-btn color="success">Buy</v-btn>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: 'BMW', price: 110 },
-        { id: 2, name: 'Google', price: 200 },
-        { id: 3, name: 'Apple', price: 250 },
-        { id: 4, name: 'Twitter', price: 210 },
-        { id: 5, name: 'Amazon', price: 300 }
-      ]
-    }
-  }
+  data: () => ({
+    stocks: [
+      { id: 1, name: 'BMW', price: 110 },
+      { id: 2, name: 'Google', price: 200 },
+      { id: 3, name: 'Apple', price: 250 },
+      { id: 4, name: 'Twitter', price: 210 },
+      { id: 5, name: 'Amazon', price: 300 }
+    ]
+  })
 }
 </script>
 
@@ -37,6 +34,7 @@ export default {
 .stocks-dashboard {
   .card {
     width: 250px;
+    margin: 10px;
     .header {
       background-color: green;
       color: white;
