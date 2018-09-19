c18d09ddbeaeb8375a69f0ad562bfb7870ce71c9
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-18 22:37:28 -0400

    Add Portfolio stock component structure

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>The portfolio - stock component</h1>
+    <v-flex>
+        <v-card class="card">
+            <div class="header">
+                {{ stock.name}} (Price: {{stock.price}} | {{stock.quantity}})
+            </div>
+            <v-flex ma-3>
+                <v-text-field label="Quantity" type="number" v-model="quantity"></v-text-field>
+            </v-flex>
+            <v-btn color="success" @click="sellStock" :disabled="quantity <=0">Sell</v-btn>
+        </v-card>
+    </v-flex>
 </template>
+
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  props: ['stock'],
+  data: () => ({ quantity: 0 }),
+  methods: {
+    ...mapActions(['sellStock']),
+    sellStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      }
+      this.sellStock()
+    }
+  }
+}
+</script>
