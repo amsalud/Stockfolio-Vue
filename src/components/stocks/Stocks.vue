3abe049939c7f65f2396ba64ef499e8a1f9423a1
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2018-09-18 22:11:16 -0400

    Add computed method to retrieve stocks from the store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -10,17 +10,13 @@
 import Stock from './Stock'
 
 export default {
-  data: () => ({
-    stocks: [
-      { id: 1, name: 'BMW', price: 110 },
-      { id: 2, name: 'Google', price: 200 },
-      { id: 3, name: 'Apple', price: 250 },
-      { id: 4, name: 'Twitter', price: 210 },
-      { id: 5, name: 'Amazon', price: 300 }
-    ]
-  }),
   components: {
     appStock: Stock
+  },
+  computed: {
+    stocks() {
+      return this.$store.getters.stocks
+    }
   }
 }
 </script>
